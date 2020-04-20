705c4de927101b2f15cc162d71c864f1c7a40918
Cuan <33520989+cuanfraser@users.noreply.github.com>
2020-04-19 21:46:42 -0400

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+var connectionString = 'mongodb://localhost:27017/cs4550-project';
+if (process.env.MLAB_USERNAME_WEBDEV) {
+    var username = process.env.MLAB_USERNAME_WEBDEV;
+    var password = process.env.MLAB_PASSWORD_WEBDEV;
+    connectionString = 'mongodb://' + username + ':' + password;
+    connectionString += '@ds111598.mlab.com:11598/heroku_tr979zdm';
+}
+
+mongoose.connect(connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin",

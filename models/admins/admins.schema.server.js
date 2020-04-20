319df46239728f5a62954927c92d190d0a40e2bc
Cuan <33520989+cuanfraser@users.noreply.github.com>
2020-04-20 14:20:08 -0400

    Validate Email fixes

--- models/admins/admins.schema.server.js
+++ models/admins/admins.schema.server.js
@@ -1,4 +1,10 @@
 const mongoose = require('mongoose')
+
+const checkEmail = (email) => {
+    var chars = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return chars.test(email)
+};
+
 const adminsSchema = mongoose.Schema({
     username: { type: String, required: true, unique: true, dropDups: true },
     password: { type: String, required: true },
@@ -7,8 +13,3 @@ const adminsSchema = mongoose.Schema({
     email: { type: String, lowercase: true, validate: [checkEmail, 'Email invalid'] },
 }, { collection: 'admins' })
 module.exports = adminsSchema
-
-const checkEmail = (email) => {
-    var chars = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    return chars.test(email)
-};

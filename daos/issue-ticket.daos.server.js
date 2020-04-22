38840b0d9953ceaa05686032649072ed28baabe9
Cuan <33520989+cuanfraser@users.noreply.github.com>
2020-04-22 15:00:52 -0400

    Find issues for repo

--- daos/issue-ticket.daos.server.js
+++ daos/issue-ticket.daos.server.js
@@ -1,4 +1,5 @@
 const issueModel = require('../models/issue-ticket/issue-ticket.model.server')
+const repoModel = require('../models/repository/repository.model.server')
 
 const findIssueById = (id) => issueModel.findOne({ _id: id })
 const findAllIssues = () => issueModel.find()
@@ -8,6 +9,6 @@ const updateIssue = (id, updateIssue) =>
     issueModel.findOneAndUpdate({ _id: id }, updateIssue)
 const deleteIssue = (id) =>
     issueModel.deleteOne({ _id: id })
-const findIssuesForRepo = (rid) => issueModel.find({ repository: rid }).populate('repository', 'name _id')
+const findIssuesForRepo = (rid) => repoModel.findById(rid).populate('issueTickets')
 
 module.exports = { findIssueById, createIssue, findAllIssues, updateIssue, deleteIssue, findIssuesForRepo }
